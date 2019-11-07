ce111161baee84e36db92fa164ae89cfc968629c
Pau <paula_ale_pe@hotmail.com>
2019-11-07 16:50:04 -0500

    Updates

---
+++ precache-manifest.f6958da7ea16c54e338d74acc2d43652.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bce158b6d5d1523c43121fca51b6159",
+    "url": "/Taller1WebAvanzada/index.html"
+  },
+  {
+    "revision": "943705731d175660f1df",
+    "url": "/Taller1WebAvanzada/static/js/2.14ca841f.chunk.js"
+  },
+  {
+    "revision": "48e162adf645624cf262",
+    "url": "/Taller1WebAvanzada/static/js/main.801bcea2.chunk.js"
+  },
+  {
+    "revision": "3b6564ccdf39b4b350c3",
+    "url": "/Taller1WebAvanzada/static/js/runtime-main.24d42560.js"
+  }
+]);
